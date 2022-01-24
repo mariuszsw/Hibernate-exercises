2a09fc4e242cfbe8a06bf5f49182e61ecb12a17e
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2022-01-24 01:48:18 +0100

    update entity

--- src/main/java/App01Update.java
+++ src/main/java/App01Update.java
@@ -1,28 +1,33 @@
 import entity.Product;
-import entity.ProductType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
-public class App01Read {
+public class App01Update {
 
-    private static final Logger logger = LogManager.getLogger(App01Read.class);
+    private static final Logger logger = LogManager.getLogger(App01Update.class);
     private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
 
     public static void main(String[] args) {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
 
-        Product product = em.find(Product.class, 1L);
+//        Product product = em.find(Product.class, 1L);
+//        product.setName("Nowy Rower 01");
+//        logger.info(product);
 
-        logger.info(product);
+//        Product merge = em.merge(product);
+//        logger.info(merge);
+
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Nowy Rower 02");
+        Product merge = em.merge(product);
+        logger.info(merge);
 
         em.getTransaction().commit();
         em.close();
-
     }
 }
