32bf30b2fef2aa98deb5d6736edb5d84d5320afe
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2022-01-27 13:37:19 +0100

    delete many to many

--- src/main/java/App12DeleteOneToOne.java
+++ src/main/java/App12DeleteOneToOne.java
@@ -2,30 +2,27 @@ import entity.Attribute;
 import entity.Product;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.w3c.dom.Attr;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class App11ManyToMany {
+public class App12DeleteOneToOne {
 
-    private static final Logger logger = LogManager.getLogger(App11ManyToMany.class);
+    private static final Logger logger = LogManager.getLogger(App12DeleteOneToOne.class);
     private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
 
     public static void main(String[] args) {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
 
-       Product product = em.find(Product.class, 5L);
-//       Attribute attribute = em.find(Attribute.class, 1L);
-//       product.addAttributes(attribute);
+       Product product = em.find(Product.class, 3L);
 
-        Attribute attribute = new Attribute();
-        attribute.setName("COLOR");
-        attribute.setValue("BLACK");
+       if (product.getCategory().getProduct().size() == 1) {
+           em.remove(product.getCategory());
+       }
+        product.setCategory(null);
 
-        product.addAttributes(attribute);
 
         em.getTransaction().commit();
         em.close();
